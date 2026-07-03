1eb60a6b940133e4047405df190b1e70273e80f9
StealthMoud <StealthMoud@gmail.com>
2026-07-03 03:37:57 +0200

    Feat: handle syntax card type and modular storage in practice

--- popup/js/practice.js
+++ popup/js/practice.js
@@ -1,9 +1,9 @@
-import { setOnDeckUpdated, getDueCards, getReviewedWordIds, getPracticeMode } from './practice/state.js';
+import { setOnDeckUpdated, getDueCards, getReviewedWordIds, getPracticeMode, hasReviewedWord, refreshReviewedWordDay, clearReviewedWords } from './practice/state.js';
 import { loadPracticeDeck, syncPracticeDeck } from './practice/card.js';
 import { registerPracticeListeners } from './practice/listeners.js';
 import { registerKeydowns } from './practice/keydowns.js';
 
-export { syncPracticeDeck, loadPracticeDeck, getDueCards, getReviewedWordIds, getPracticeMode };
+export { syncPracticeDeck, loadPracticeDeck, getDueCards, getReviewedWordIds, getPracticeMode, hasReviewedWord, refreshReviewedWordDay, clearReviewedWords };
 
 export async function initPractice(onDeckUpdated) {
   setOnDeckUpdated(onDeckUpdated);
